ff80be53e27d13d2fe24f8e7b5e59e544f177094
[dyad] <guiroesler2@gmail.com>
2025-10-08 22:42:04 -0300

    [dyad] Refatorando o CSS em módulos - wrote 7 file(s), deleted 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './App.css'
+import './styles/globals.css'
+import './styles/animations.css'
+import './styles/components/toolbar.css'
+import './styles/components/settings.css'
+import './styles/components/side-panel.css'
+import './styles/components/info-card.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
